1a79e9eff566a3a30c125ccddd62c9cb50f3379e
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2023-08-30 14:35:48 +0300

    minor style fixes

--- src/atoms/TodoItem/TodoItem.jsx
+++ src/atoms/TodoItem/TodoItem.jsx
@@ -4,7 +4,12 @@ import React from 'react';
 import styles from './style.module.scss';
 
 export const TodoItem = ({ task }) => {
-  return <div className={styles.todoItem}>{task.title}</div>;
+  return (
+    <div className={styles.todoItem}>
+      <b className={styles.todoItemId}>{task.id}</b>
+      <p>{task.title}</p>
+    </div>
+  );
 };
 
 TodoItem.propTypes = {
--- src/organisms/TodoColumn/TodoColumn.jsx
+++ src/organisms/TodoColumn/TodoColumn.jsx
@@ -8,12 +8,11 @@ import { TodoItem } from '../../atoms';
 export const TodoColumn = ({ column }) => {
   return (
     <div className={styles.columnWrapper}>
-      <h2>
-        {column.title}
-        {column.tasks.length > 0 && <span>: {column.tasks.length}</span>}
-      </h2>
-
       <div className={styles.column}>
+        <h3 className={styles.columnTitle}>
+          {column.title}
+          {column.tasks.length > 0 && <span>: {column.tasks.length}</span>}
+        </h3>
         {column.tasks.map((task) => (
           <Fragment key={task.id}>
             <TodoItem task={task} />
